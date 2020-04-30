e964d0b241f5c8a899612d1976202e0790f3d786
MGCSousa <manuelgcsousa@gmail.com>
2020-04-30 19:18:14 +0100

    help

--- commands/help.js
+++ commands/help.js
@@ -2,15 +2,15 @@ module.exports = {
 	name: 'help',
 	description: 'List all of the available commands or info about a specific command.',
 	execute(message, args) {
-		const data = [];
+		var msg = "";
 		const { commands } = message.client;
 
 		if (args.length === 0) {
-			data.push('Here\'s a list of all the commands:');
-			data.push(commands.map(command => command.name).join(', '));
-			data.push('\nYou can do $help [command name] to get info on a specific command!');
+			msg += 'Here\'s a list of all the commands: ';
+			msg += (commands.map(command => command.name).join(', '));
+			msg += '\nYou can do \'$help [command name]\' to get info on a specific command!';
 
-			return message.author.send(data, { split: true });
+			return message.channel.send(msg);
 		}
 	}
 };
